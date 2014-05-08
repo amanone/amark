f16fa641ddf59869b19325360fefa7d35dfb6673
Thomas Coudray <thomas.coudray@bromium.com>
2014-05-08 18:21:12 +0100

    detector: WIP: update get_vmlinux_symbol.sh

--- detector/get_vmlinux_symbol.sh
+++ detector/get_vmlinux_symbol.sh
@@ -1,3 +1,5 @@
+## DO NOT EXECUTE THIS FILE
+
 cp -v /vmlinuz .
 file vmlinuz
 ./extract-vmlinux vmlinuz > vmlinux
@@ -5,3 +7,10 @@ file vmlinux
 objdump -D vmlinux -M intel > disass_intel_vmlinux
 echo 'disassembled in disass_intel_vmlinux'
 dwarfdump -di vmlinux  2> dwarf_vmlinux
+# Our kernel is not compiled with RELOCATED
+# and RANDOMIZE* (i.e. KALSR)
+# So I think it might work...
+
+grep  -e ' sys_open$' /boot/System.map-3.2*
+cd lime && make
+insmod lime-*.ko "path=/vagrant/git/detector/memory_dump format=lime"
